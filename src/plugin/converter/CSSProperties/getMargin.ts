83bb28a744ba51df4d568d5c81f083bdaf1ec7c5
toshusai <toshusai.net@gmail.com>
2023-04-30 16:13:58 +0900

    fix: text margin

--- src/plugin/converter/CSSProperties/getMargin.ts
+++ src/plugin/converter/CSSProperties/getMargin.ts
@@ -3,8 +3,8 @@ import { getPadding } from './getPadding';
 
 export function getMargin(node: FrameNode | InstanceNode | RectangleNode | TextNode): CSSProperties {
   if (node.type === 'TEXT' && node.parent !== null) {
-    if (node.parent.type === 'FRAME' || node.parent.type === 'INSTANCE') {
-      const padding = getPadding(node.parent, { isText: true });
+    if ('paddingTop' in node.parent) {
+      const padding = getPadding(node.parent as FrameNode | InstanceNode, { isText: true });
       return {
         margin: padding.padding,
         marginTop: padding.paddingTop,
