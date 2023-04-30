fe490553b3f036ceee7aa8f04509744a5d223f16
toshusai <toshusai.net@gmail.com>
2023-04-30 16:52:55 +0900

    fix: borde

--- src/plugin/converter/CSSProperties/getBorder.ts
+++ src/plugin/converter/CSSProperties/getBorder.ts
@@ -4,12 +4,16 @@ import { isMixed, rgbColorToHex } from '../../utils';
 export function getBorder(node: SceneNode): CSSProperties {
   if ('strokes' in node) {
     if (isMixed(node.strokes)) return {};
-    if (isMixed(node.strokeWeight)) return {};
     if (node.strokes.length > 0) {
       const stroke = node.strokes[0];
       if (stroke.type === 'SOLID') {
+        const color = rgbColorToHex(stroke.color, stroke.opacity);
         return {
-          border: `${rgbColorToHex(stroke.color, stroke.opacity)} ${node.strokeWeight}px solid`,
+          border: isMixed(node.strokeWeight) ? undefined : `${color} ${node.strokeWeight}px solid`,
+          borderTop: 'strokeTopWeight' in node ? `${color} ${node.strokeTopWeight}px solid` : undefined,
+          borderRight: 'strokeRightWeight' in node ? `${color} ${node.strokeRightWeight}px solid` : undefined,
+          borderBottom: 'strokeBottomWeight' in node ? `${color} ${node.strokeBottomWeight}px solid` : undefined,
+          borderLeft: 'strokeLeftWeight' in node ? `${color} ${node.strokeLeftWeight}px solid` : undefined,
         };
       }
     }
