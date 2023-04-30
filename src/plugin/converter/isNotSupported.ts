f997f538fa4667b2cac09c63835b675c90af2678
toshusai <toshusai.net@gmail.com>
2023-04-30 17:53:56 +0900

    fix: padding

--- src/plugin/converter/isNotSupported.ts
+++ src/plugin/converter/isNotSupported.ts
@@ -2,7 +2,11 @@ export function isNotSupported(node: SceneNode) {
   if ('children' in node) {
     const hasText = node.children.find((child) => child.type === 'TEXT');
     const hasPadding =
-      'paddingTop' in node || 'paddingBottom' in node || 'paddingLeft' in node || 'paddingRight' in node;
+      ('paddingTop' in node && node.paddingTop !== 0) ||
+      ('paddingBottom' in node && node.paddingBottom !== 0) ||
+      ('paddingLeft' in node && node.paddingLeft !== 0) ||
+      ('paddingRight' in node && node.paddingRight !== 0);
+
     if (hasPadding && hasText && node.children.length > 1) {
       console.warn(`The node ${node.name} has padding and text. This is not supported.`);
     }
