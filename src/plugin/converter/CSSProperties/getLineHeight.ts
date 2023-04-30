46a916929a2f217461959791997e13458c8afc1f
toshusai <toshusai.net@gmail.com>
2023-04-30 17:53:50 +0900

    fix: line-height

--- src/plugin/converter/CSSProperties/getLineHeight.ts
+++ src/plugin/converter/CSSProperties/getLineHeight.ts
@@ -3,7 +3,7 @@ import { CSSProperties } from 'react';
 export function getLineHighlight(node: SceneNode): CSSProperties {
   if (node.type === 'TEXT') {
     return {
-      lineHeight: `${node.height}px`,
+      lineHeight: typeof node.fontSize === 'number' ? `${Math.round(node.fontSize * 1.2)}px` : undefined,
     };
   }
 
