dfa7246d61b99ba3d873fe02e827dc7dfcad5d6f
toshusai <toshusai.net@gmail.com>
2023-05-01 12:19:15 +0900

    faet: optimize border

--- src/plugin/converter/CSSProperties/getBorder.ts
+++ src/plugin/converter/CSSProperties/getBorder.ts
@@ -8,16 +8,30 @@ export function getBorder(node: SceneNode): CSSProperties {
       const stroke = node.strokes[0];
       if (stroke.type === 'SOLID') {
         const color = rgbColorToHex(stroke.color, stroke.opacity);
-        return {
+        const botder = {
           border: isMixed(node.strokeWeight) ? undefined : `${color} ${node.strokeWeight}px solid`,
           borderTop: 'strokeTopWeight' in node ? `${color} ${node.strokeTopWeight}px solid` : undefined,
           borderRight: 'strokeRightWeight' in node ? `${color} ${node.strokeRightWeight}px solid` : undefined,
           borderBottom: 'strokeBottomWeight' in node ? `${color} ${node.strokeBottomWeight}px solid` : undefined,
           borderLeft: 'strokeLeftWeight' in node ? `${color} ${node.strokeLeftWeight}px solid` : undefined,
         };
+        return optimizeBorderStyle(botder);
       }
     }
   }
 
   return {};
+}
+
+function optimizeBorderStyle(style: CSSProperties): CSSProperties {
+  if (
+    style.borderTop === style.borderRight &&
+    style.borderTop === style.borderBottom &&
+    style.borderTop === style.borderLeft
+  ) {
+    return {
+      border: style.borderTop,
+    };
+  }
+  return style;
 }
