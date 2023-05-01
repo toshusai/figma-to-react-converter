0d8c7515b0bb3eccff122fcc980bd12b44cb9d6c
toshusai <toshusai.net@gmail.com>
2023-05-01 16:50:27 +0900

    refactor: use generated code

--- src/app/components/example/design/HeaderLayout.tsx
+++ src/app/components/example/design/HeaderLayout.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+// custom code
 import { TextButton } from './TextButton';
 
-type DesignProps = {
-  textButtonProps: Parameters<typeof TextButton>[0];
-  children?: React.ReactNode;
+type StyledProps<T> = Omit<React.DetailedHTMLProps<React.HTMLAttributes<T>, T>, 'children' | 'ref'> & {
+  ref?: React.Ref<T>;
 };
 
-export function HeaderLayout(props: DesignProps) {
+export type HeaderLayoutProps = {
+  children: React.ReactNode;
+  styledHeaderLayoutProps?: StyledProps<HTMLDivElement>;
+  // custom code
+  textButtonProps?: Parameters<typeof TextButton>[0];
+};
+
+export function HeaderLayout(props: HeaderLayoutProps) {
   return (
-    <div className="wrap">
-      <TextButton {...props.textButtonProps}></TextButton>
+    <StyledHeaderLayout {...props.styledHeaderLayoutProps}>
+      <TextButton
+        {...props.textButtonProps}
+        // custom code
+        text="Back"
+      />
       {props.children}
-    </div>
+    </StyledHeaderLayout>
   );
 }
+
+export const StyledHeaderLayout = styled.div`
+  background-color: #ffffffff;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  padding: 4px 16px;
+  position: relative;
+  border-radius: 0px;
+  overflow: hidden;
+  align-items: center;
+  justify-content: flex-start;
+`;
