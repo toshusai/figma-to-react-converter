f9de57f69cd56a23a0544e334cd5ac5c34bc6de6
toshusai <toshusai.net@gmail.com>
2023-05-01 17:11:35 +0900

    refactor: remove unused

--- src/app/components/Tabs/internal/Tab.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react';
-
-export function Tab(props: { children: ReactNode; onClick?: () => void; selected?: boolean }) {
-  return (
-    <button onClick={props.onClick} className={`tab ${props.selected ? 'tab-selected' : ''}`}>
-      {props.children}
-    </button>
-  );
-}
