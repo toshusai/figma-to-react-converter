e814148b2975b6fcca943fdd0b35f8a056bb2f09
toshusai <toshusai.net@gmail.com>
2023-05-01 16:39:08 +0900

    feat: add copy button

--- src/app/components/Code.tsx
+++ src/app/components/Code.tsx
@@ -1,13 +1,46 @@
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode, useEffect, useRef } from 'react';
 import hljs from 'highlight.js';
+import styled from 'styled-components';
 
 export function Code(props: { lang: string; children: ReactNode }): JSX.Element {
   useEffect(() => {
     hljs.highlightAll();
   }, []);
+  const codeRef = useRef<HTMLElement>(null);
+  const copy = () => {
+    if (!props.children) return;
+    const codeTag = codeRef.current;
+    if (!codeTag) return;
+    const range = document.createRange();
+    range.selectNode(codeTag);
+    window.getSelection()?.addRange(range);
+    document.execCommand('copy');
+    window.getSelection()?.removeAllRanges();
+  };
+
   return (
-    <pre>
-      <code className={`language-${props.lang}`}>{props.children}</code>
+    <pre
+      style={{
+        position: 'relative',
+      }}
+    >
+      <CopyButton onClick={copy}>Copy</CopyButton>
+      <code ref={codeRef} className={`language-${props.lang}`}>
+        {props.children}
+      </code>
     </pre>
   );
 }
+
+const CopyButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  background: none;
+  color: white;
+  border: 1px solid white;
+  font-size: 16px;
+  padding: 8px;
+  cursor: pointer;
+`;
